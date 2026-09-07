ccabf09fbcf0dbe1718b50b6345ef166b2ded626
RJ / SaeDus <xRJ.Prototype@gmail.com>
2026-09-07 16:26:37 -0600

    Socketed Item Pass
    Removed the code for socketed items since the BaseName is the only necessary element at this point.

--- exporters/diablo-2/Models/SocketState.cs
+++ exporters/diablo-2/Models/SocketState.cs
@@ -1,6 +1,10 @@
-public class SocketState {
+using System.Text.Json.Serialization;
+
+public class SocketState
+{
     public bool IsEmpty { get; set; }
 
+    [JsonIgnore]
     public string? BaseCode { get; set; }
     public string? BaseName { get; set; }
 }
