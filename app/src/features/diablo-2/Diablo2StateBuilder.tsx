2cff1a86b32546e6fe642191053aae726a7ea639
RJ / SaeDus <56566069+SaeDus@users.noreply.github.com>
2026-09-17 14:22:12 -0600

    Established Blank Diablo 2 Style

--- app/src/features/diablo-2/Diablo2StateBuilder.tsx
+++ app/src/features/diablo-2/Diablo2StateBuilder.tsx
@@ -1,6 +1,8 @@
 import Diablo2Main from "./Diablo2Main";
 import Diablo2Exporter from "./Diablo2Exporter";
 
+import "./styles/Diablo2.css";
+
 function Diablo2StateBuilder() {
   return <Diablo2Main />
 }
